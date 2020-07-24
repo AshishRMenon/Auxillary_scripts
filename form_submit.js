364332fdae55edfe3b3de4770a46c57aeec54b05
ashish95 <ashishmenon95@gmail.com>
2020-07-25 01:28:41 +0530

    added the form on submit backend script

---
+++ form_submit.js
@@ -0,0 +1,68 @@
+function sendFormByEmail(e) 
+{    
+  
+  var items = e.response.getItemResponses();
+  for(var i in items){
+    if (items[i].getItem().getTitle()=="Advisor Email Address"){
+      var adv_address = items[i].getResponse();
+    }
+    }
+  
+  
+  data = {}  
+  var headers = [
+    "Preferred Username",
+    "Name",
+    "Email Address",
+    "Advisor Name",
+    "Compute Level",
+  ]
+  data["Compute Level"] = "L1";
+  for(var i in items){
+    //Logger.log(i);
+    var key = items[i].getItem().getTitle();
+    var value = items[i].getResponse();
+    if (items[i].getItem().getTitle()=="Advisor Email Address"){ continue; }
+    data[key] = value;
+  }
+  
+  var message = "";
+  var subject = "[ada][cvit-account] ";
+  
+  e = {
+    namedValues: data
+  }
+  
+  var entries = [];
+  for(var i in headers){
+    Logger.log(e.namedValues[headers[i]].toString())
+    var wrapped = /* '"' + */ e.namedValues[headers[i]].toString() /* +  '"' */;
+    entries.push(wrapped);    
+  }
+  
+  var values = entries.join(",")
+  var name = headers[1]
+  subject += e.namedValues[name].toString() ;
+  message += /* headers.join(",") + "\n" +*/ values + "\n";
+
+  //Logger.log(entries);
+  //Logger.log(values);
+  
+  to = "hpc@iiit.ac.in";
+  cc = data["Email Address"] + ',' + "cvit-sudo@googlegroups.com" + ',' + adv_address;
+  
+  //to = "ashishmenon95@gmail.com";
+  //cc = data["Email Address"] + ',' + adv_address + ',' + "ashish.r.menon@gmail.com";
+  
+  var csvSegment = "<pre>" + message + "</pre>";
+  
+  MailApp.sendEmail({
+    to: to,
+    cc: cc,
+    subject:  subject,
+    htmlBody: csvSegment,
+  })
+  
+  // MailApp.sendEmail(email, subject, message); 
+
+}
